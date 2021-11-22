9677bd443da1e6593c7abffb864d7ee85d25182c
Yana Sokurenko <yanamoss@gmail.com>
2021-11-22 17:38:00 +0200

    done

--- src/js/getImages.js
+++ src/js/getImages.js
@@ -1,12 +1,11 @@
-'use strict'
+'use strict';
 const axios = require('axios');
 export const getImages = async (requestName, page, limit) => {
-   const baseUrl = `https://pixabay.com/api/?key=22644758-91a56f4647f302f87ea071930&q=${requestName}&page=${page}&per_page=${limit}&image_type=photo&orientation=horizontal&safesearch=true`;
-   if (!requestName || requestName.trim() === '') {
-      return [];
-   };
-   
-   const response = await axios.get(baseUrl);
-   return response.data;
-};
+  const baseUrl = `https://pixabay.com/api/?key=22644758-91a56f4647f302f87ea071930&q=${requestName}&page=${page}&per_page=${limit}&image_type=photo&orientation=horizontal&safesearch=true`;
+  if (!requestName || requestName.trim() === '') {
+    return [];
+  }
 
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
